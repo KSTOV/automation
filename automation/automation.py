f66686e29162d294f9f8773db953aa2738dbc4df
KLan Stovall <kland13@gmail.com>
2021-12-26 20:42:48 -0800

    lab complete

--- automation/automation.py
+++ automation/automation.py
@@ -5,11 +5,21 @@ def get_info(file_path):
         file_text = f.read()
 
     phone_pattern = r"\d{3}-\d{3}-\d{4}"
-    email_pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+    email_pattern = r"\w*@\w*.\w*"
 
-    results = re.findall(pattern, file_text)
+    phone_results = re.findall(phone_pattern, file_text)
+    email_results = re.findall(email_pattern, file_text)
 
-    print(results)
+    sorted_phone_results = sorted(phone_results)
+    sorted_email_results = sorted(email_results)
+
+    with open('automation/phone_numbers.txt', 'w') as f:
+        for number in sorted_phone_results:
+            f.write(f"{str(number)}\n")
+
+    with open('automation/emails.txt', 'w') as f:
+        for email in sorted_email_results:
+            f.write(f"{str(email)}\n")
 
 if __name__ == "__main__":
-    get_info('./potential-contacts.txt')
+    get_info('automation/potential-contacts.txt')
