e19e785cfe07e7fdab2ec6368edb2fe79378ccfe
KLan Stovall <kland13@gmail.com>
2021-12-26 21:27:54 -0800

    updated README and fixed some errors

--- automation/automation.py
+++ automation/automation.py
@@ -4,14 +4,14 @@ def get_info(file_path):
     with open(f'{file_path}') as f:
         file_text = f.read()
 
-    phone_pattern = r"\d{3}-\d{3}-\d{4}"
-    email_pattern = r"\w*@\w*.\w*"
+    phone_pattern = r"(\d{3}[-]\d{3}[-]\d{4}|\(\d{3}\)\s*\d{3}[-]??\d{4})"
+    email_pattern = r"[a-zA-Z0-9\-_\.]+@[a-zA-Z0-9\-_\.]+\.[a-zA-Z]{2,5}"
 
     phone_results = re.findall(phone_pattern, file_text)
     email_results = re.findall(email_pattern, file_text)
 
-    sorted_phone_results = sorted(phone_results)
-    sorted_email_results = sorted(email_results)
+    sorted_phone_results = sorted(list(dict.fromkeys(phone_results)))
+    sorted_email_results = sorted(list(dict.fromkeys(email_results)))
 
     with open('automation/phone_numbers.txt', 'w') as f:
         for number in sorted_phone_results:
